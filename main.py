343ddc88afb7f8c7478a705130d2e79727e5ec5c
Luka Milinkovic <luka.milinkovic@icloud.com>
2026-06-17 21:48:30 +0200

    Refactor email and birthday logic in main.py

--- main.py
+++ main.py
@@ -7,32 +7,39 @@
 
 
 from datetime import datetime
-import pandas
+import pandas as pd
 import random
 import smtplib
-import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+MY_EMAIL = os.environ.get("luka.milinkovic789@gmail.com")
+PASSWORD = os.environ.get("iwzx ubtq ugky ozdv")
+
+
 
 today = datetime.now()
-today_tuple = (today.month, today.day)
+today_month = today.month
+today_day = today.day
+today_tuple = (today_month, today_day)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+data = pd.read_csv("birthdays.csv")
+birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if today_tuple in birthday_dict:
+    birthday_person = birthday_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
     with open(file_path) as letter_file:
         contents = letter_file.read()
         contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.login(MY_EMAIL, PASSWORD)
         connection.sendmail(
             from_addr=MY_EMAIL,
             to_addrs=birthday_person["email"],
             msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+            )
+
+
+
